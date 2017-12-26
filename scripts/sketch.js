4bf98c43736cb1e1fcf0d4204a24e0b3c0968818
aunaun <dekdekbaloo@gmail.com>
2017-12-26 13:18:35 +0700

    Set up socket injected from devServer

---
+++ scripts/sketch.js
@@ -0,0 +1,19 @@
+function Segment (x, y, len, angle) {
+  this.a = createVector(x, y)
+  this.len = len
+
+  this.update = function () {
+
+  }
+
+  this.draw = function () {
+    
+  }
+}
+
+function setup () {
+  console.log('setup')
+}
+function draw () {
+  
+}
